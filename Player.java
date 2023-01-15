eca81cbbca5a712103efff4601b6c83b37703238
anushka344 <73011298+anushka344@users.noreply.github.com>
2023-01-15 20:36:39 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,65 @@
+import java.util.*; 
+class Player 
+{ private int playerId; 
+ private String playerName;
+ private int runs; 
+ private String playerType; 
+ private String matchType;
+ Player(int playerId, String playerName, int runs, String playerType, String matchType)
+ { this.playerId= playerId; 
+  this.playerName= playerName;
+  this.runs=runs;
+  this.playerType= playerType; 
+  this.matchType= matchType;
+ }
+ public int getplayerId()
+ { 
+   return this.playerId; 
+ } 
+ public String getplayerName()
+ {
+   return this.playerName;
+ }
+ public int getruns()
+ {
+   return this.runs;
+ }
+ public String getplayerType()
+ {
+   return this.playerType;
+ }
+ public String getmatchType()
+ {
+   return this.matchType;
+ }
+}
+public class MyClass 
+{
+  public static int findPlayerWithLowestRuns (Player[] player , String type1)
+  {
+    int lowest=Integer.MAX_VALUE; 
+    for (int i=0;i< help[j+1].getplayerId())
+    {
+      Player temp=help[j+1];
+      help[j+1]=help[j]; 
+      help[j]=temp; } 
+  }
+}
+if(help.length==0) return null;
+else
+  return help; 
+} 
+public static void main(String[] args)
+{
+  int playerId; 
+  String playerName;
+  int runs; 
+  String playerType;
+  String matchType;
+  Player[] player= new Player[4]; 
+  Scanner sc = new Scanner(System.in);
+  for (int i=0;i<ans2.length;i++)
+    System.out.println(ans2[i].getplayerId());
+}
+}
+}
